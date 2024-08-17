10ee3fb899bdae95c0970ca9210fd21870902567
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-08-17 16:42:42 +0600

    products paggination updated

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,68 +1,95 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FaArrowLeftLong, FaArrowRightLong } from 'react-icons/fa6';
 import useAxiosCommon from '../../hooks/useAxiosCommon';
 import Product from '../Product/Product';
 
 const Products = () => {
-    const [itemsPerPage] = useState(9);
-    const [currentPage, setCurrentPage] = useState(1)
-    const [count, setCount] = useState(0);
-    const [filter, setFilter] = useState('');
-    const [sort, setSort] = useState('');
     const axiosCommon = useAxiosCommon();
 
-    const { data: pens, isLoading } = useQuery({
-        queryKey: ['pens'],
+    const [size] = useState(6);
+    const [currentPage, setCurrentPage] = useState(1)
+    // const [count, setCount] = useState(40);
+    // const [filter, setFilter] = useState('');
+    const [brand, setBrand] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
+    const [date, setDate] = useState('');
+    const [sortPrice, setSortPrice] = useState('');
+    const [search, setSearch] = useState('');
+
+    const { data } = useQuery({
+        queryKey: ['pens', size, currentPage, brand, category, price, date, sortPrice, search],
         queryFn: async () => {
-            const res = await axiosCommon.get(`/pens?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}`)
+            const res = await axiosCommon.get(`/pens`, {
+                params: {
+                    size,
+                    page: currentPage,
+                    brand,
+                    category,
+                    price,
+                    date,
+                    sortPrice,
+                    search
+                }
+            })
             return res.data;
         }
     })
 
+
     // useEffect(() => {
-    //     getData();
-    // }, [currentPage, filter, itemsPerPage, sort])
-    // const getData = async () => {
-    //     const { data } = await axiosCommon.get(`/all-pens?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}`)
-    //     setBooks(data);
-    // }
+    //     const getCount = async () => {
+    //         const { data } = await axiosCommon.get(`/pens-count?filter=${filter}`)
+    //         setCount(data.count);
+    //     }
+    //     getCount();
+    // }, [filter])
+    // const numberOfPages = Math.ceil(pens.length / size)
+    // const pages = [...Array(numberOfPages).keys()].map(e => e + 1)
 
-    useEffect(() => {
-        const getCount = async () => {
-            const { data } = await axiosCommon.get(`/pens-count?filter=${filter}`)
-            setCount(data.count);
-        }
-        getCount();
-    }, [filter])
-
-    const numberOfPages = Math.ceil(count / itemsPerPage)
-    const pages = [...Array(numberOfPages).keys()].map(e => e + 1)
+    const pens = data?.pens || [];
+    const totalPages = data?.totalPages || 1;
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
     // handlePagination btn
     const handlePaginationButton = (value) => {
         setCurrentPage(value);
     }
 
-
     return (
         <>
             <h2 className='text-2xl font-bold text-center py-6'>Total Products: {pens?.length}</h2>
-
             <div className='flex flex-col md:flex-row justify-center items-center gap-5 mb-6'>
-                {/* filter */}
+                {/* brand */}
                 <div>
                     <select
                         onChange={e => {
-                            setFilter(e.target.value)
+                            setBrand(e.target.value)
                             setCurrentPage(1)
                         }}
-                        value={filter}
-                        name='category'
-                        id='category'
+                        value={brand}
                         className='border p-4 rounded-lg'
                     >
-                        <option value=''>Filter By Category</option>
+                        <option value=''>Filter By Brand</option>
+                        <option value='TechMark'>TechMark</option>
+                        <option value='EliteTech'>EliteTech</option>
+                        <option value='MajestyMobile'>MajestyMobile</option>
+                        <option value='GoldTech'>GoldTech</option>
+                    </select>
+                </div>
+
+                {/* category */}
+                <div>
+                    <select
+                        onChange={e => {
+                            setCategory(e.target.value)
+                            setCurrentPage(1)
+                        }}
+                        value={category}
+                        className='border p-4 rounded-lg'
+                    >
+                        <option value=''>Filter By category</option>
                         <option value='Luxury'>Luxury</option>
                         <option value='High'>High</option>
                         <option value='Medium'>Medium</option>
@@ -70,34 +97,67 @@ const Products = () => {
                     </select>
                 </div>
 
-                {/* sort */}
+                {/* price */}
                 <div>
                     <select
                         onChange={e => {
-                            setSort(e.target.value)
+                            setPrice(e.target.value)
                             setCurrentPage(1)
                         }}
-                        value={sort}
-                        name='sort'
-                        id='sort'
+                        value={price}
+                        className='border p-4 rounded-lg'
+                    >
+                        <option value=''>Filter By price</option>
+                        <option value='0-100'>$ 0 - $ 100</option>
+                        <option value='100-200'>$ 100 - $ 200</option>
+                        <option value='200-300'>$ 200 - $ 300</option>
+                        <option value='300-400'>$ 300 - $ 400</option>
+                        <option value='400-500'>$ 400 - $ 500</option>
+                    </select>
+                </div>
+
+                {/* date */}
+                <div>
+                    <select
+                        onChange={e => {
+                            setDate(e.target.value)
+                            setCurrentPage(1)
+                        }}
+                        value={date}
                         className='border p-4 rounded-md'
                     >
-                        <option value=''>Sort by Creation Date & Time</option>
+                        <option value=''>Sort by Date</option>
+                        <option value='ascending'>Ascending Order</option>
+                        <option value='descending'>Descending Order</option>
+                    </select>
+                </div>
+
+                {/* sort price */}
+                <div>
+                    <select
+                        onChange={e => {
+                            setSortPrice(e.target.value)
+                            setCurrentPage(1)
+                        }}
+                        value={sortPrice}
+                        className='border p-4 rounded-md'
+                    >
+                        <option value=''>Sort by Price</option>
                         <option value='ascending'>Ascending Order</option>
                         <option value='descending'>Descending Order</option>
                     </select>
                 </div>
 
                 {/* search */}
-                <div className="">
-                    <input type="text" placeholder="search" className="border p-4 rounded-md" required />
-                    <button className='btn btn-primary'>Search</button>
+                <div className="space-x-3">
+                    <input value={search}
+                        onChange={(e) => setSearch(e.target.value)} type="text" placeholder="search by name" className="border p-4 rounded-md" required />
                 </div>
             </div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
-                    pens?.slice(0, 6).map((pen, idx) => <Product key={idx} pen={pen} />)
+                    pens?.map((pen, idx) => <Product key={idx} pen={pen} />)
                 }
             </div>
 
@@ -123,7 +183,7 @@ const Products = () => {
                 ))}
 
                 <button
-                    disabled={currentPage === numberOfPages}
+                    disabled={currentPage === pens.length}
                     onClick={() => handlePaginationButton(currentPage + 1)}
                     className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
                     <div className='flex items-center mx-1'>
